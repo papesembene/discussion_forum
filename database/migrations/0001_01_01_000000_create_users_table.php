49df02cae7dd81c3aeaf14a7f8e8199b77edaa08
sembene <sembenpape4@gmail.com>
2024-06-12 13:06:31 +0000

    creation des models et migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->nullable();
+            $table->string('role')->default('user');
+            $table->timestamp('last_seen')->nullable();
+
+
             $table->rememberToken();
             $table->timestamps();
         });
