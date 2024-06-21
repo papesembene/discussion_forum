845298337283572007f40c69c1b8fe7c77e0f6a1
sembene <sembenpape4@gmail.com>
2024-06-21 22:49:35 +0000

    modificaton migration tags

--- database/migrations/2024_06_12_124257_create_tags_table.php
+++ database/migrations/2024_06_12_124257_create_tags_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('slug');
             $table->string('name');
             $table->timestamps();
         });
